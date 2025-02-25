e1018f08d9ba2e272f3998654b9187cbcf85c865
ST10294518 <126757495+ST10294518@users.noreply.github.com>
2025-02-25 21:41:08 +0200

    Part 6, controller methods and views in ASP.NET Core

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ac1288d3475a4965cc6aab699a641565fa4585e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8126355659ab397758fec1a1f962eb311a8d13")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
